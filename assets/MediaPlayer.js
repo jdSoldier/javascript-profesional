3e3e9e2e0fff9635420d20feb2c7eefce86e119e
josuenc <josue.naure.c@gmail.com>
2022-07-05 10:06:07 -0500

    CLASE 07: el primer plugin

--- assets/MediaPlayer.js
+++ assets/MediaPlayer.js
@@ -1,6 +1,15 @@
 function MediaPlayer(config) {
   this.media = config.el;
+  this.plugins = config.plugins || [];
+  //damos como valor inicial en caso de no encontrar un pluggin un array vacio
+  this._initPlugins();
 }
+
+MediaPlayer.prototype._initPlugins = function () {
+  this.plugins.forEach((plugin) => {
+    plugin.run(this);
+  });
+};
 
 MediaPlayer.prototype.play = function () {
   this.media.play();
@@ -8,6 +17,14 @@ MediaPlayer.prototype.play = function () {
 
 MediaPlayer.prototype.pause = function () {
   this.media.pause();
+};
+
+MediaPlayer.prototype.mute = function () {
+  this.media.muted = true;
+};
+
+MediaPlayer.prototype.unmute = function () {
+  this.media.muted = false;
 };
 
 MediaPlayer.prototype.togglePlay = function () {
